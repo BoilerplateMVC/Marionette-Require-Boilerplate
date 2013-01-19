402dd7f4a2aa22c51d7d410c92ba35c64701babd
Brett Jones <brettjonesdev@gmail.com>
2013-01-19 16:21:32 -0600

    Updated mobile header info

--- public/js/app/views/MobileHeaderView.js
+++ public/js/app/views/MobileHeaderView.js
@@ -2,6 +2,10 @@ define(['underscore', 'jquery', 'handlebars', 'text!templates/mobileHeader.html'
     function (_, $, Handlebars, template) {
         //ItemView provides some default rendering logic
         return Backbone.Marionette.ItemView.extend({
-            template: template
+            template: template,
+
+            render: function() {
+                this.$el.html( Backbone.Marionette.Renderer.render( this.template, {} )).navbar();
+            }
         });
     });
