0bea47cdb26bf5b3b099beee686506816caa6434
Brett Jones <brettjonesdev@gmail.com>
2013-02-02 11:13:51 -0600

    Updating tests (not complete yet) and license

--- public/js/test/config/TestInit.js
+++ public/js/test/config/TestInit.js
@@ -1,102 +1,79 @@
-// TestInit.js
 require.config({
+    baseUrl:"./js",
+    // 3rd party script alias names (Easier to type "jquery" than "libs/jquery, etc")
+    // probably a good idea to keep version numbers in the file names for updates checking
+    paths:{
+        // Core Libraries
+        "jquery":"libs/jquery",
+        "jqueryui":"libs/jqueryui",
+        "jquerymobile":"libs/jquery.mobile",
+        "underscore":"libs/lodash",
+        "backbone":"libs/backbone",
+        "marionette":"libs/backbone.marionette",
+        "handlebars":"libs/handlebars",
+        "jasmine": "libs/jasmine",
+        "jasmine-html": "libs/jasmine-html",
 
-  // Sets the js folder as the base directory for all future relative paths
-  baseUrl: "./js",
+        // Plugins
+        "backbone.validateAll":"libs/plugins/Backbone.validateAll",
+        "bootstrap":"libs/plugins/bootstrap",
+        "text":"libs/plugins/text",
+        "jasminejquery": "libs/plugins/jasmine-jquery",
 
-  // 3rd party script alias names (Easier to type "jquery" than "libs/jquery, etc")
-  // probably a good idea to keep version numbers in the file names for updates checking
-  paths: {
+        // Application Folders
+        "collections":"app/collections",
+        "models":"app/models",
+        "routers":"app/routers",
+        "templates":"app/templates",
+        "config":"app/config",
+        "controllers":"app/controllers",
+        "views":"app/views"
+    },
+    // Sets the configuration for your third party scripts that are not AMD compatible
+    shim:{
+        // Twitter Bootstrap jQuery plugins
+        "bootstrap":["jquery"],
+        // jQueryUI
+        "jqueryui":["jquery"],
+        // Backbone
+        "backbone":{
+            // Depends on underscore/lodash and jQuery
+            "deps":["underscore", "jquery"],
+            // Exports the global window.Backbone object
+            "exports":"Backbone"
+        },
+        //Marionette
+        "marionette":{
+            "deps":["underscore", "backbone", "jquery"],
+            "exports":"Marionette"
+        },
+        //Handlebars
+        "handlebars":{
+            "exports":"Handlebars"
+        },
+        // Backbone.validateAll plugin that depends on Backbone
+        "backbone.validateAll":["backbone"],
 
-      // Core Libraries
-      // --------------
-      "jquery": "libs/jquery",
+        // Jasmine Unit Testing
+        "jasmine": {
 
-      "jquerymobile": "libs/jquery.mobile",
+            // Exports the global 'window.jasmine' object
+            "exports": "jasmine"
 
-      "underscore": "libs/lodash",
+        },
 
-      "backbone": "libs/backbone",
-
-      "jasmine": "libs/jasmine",
-
-      "jasmine-html": "libs/jasmine-html",
-
-      // Plugins
-      // -------
-      "backbone.validateAll": "libs/plugins/Backbone.validateAll",
-
-      "bootstrap": "libs/plugins/bootstrap",
-
-      "text": "libs/plugins/text",
-
-      "jasminejquery": "libs/plugins/jasmine-jquery",
-
-      // Application Folders
-      // -------------------
-      "collections": "app/collections",
-
-      "models": "app/models",
-
-      "routers": "app/routers",
-
-      "templates": "app/templates",
-
-      "views": "app/views"
-
-  },
-
-  // Sets the configuration for your third party scripts that are not AMD compatible
-  shim: {
-
-      // Twitter Bootstrap jQuery plugins
-      "bootstrap": ["jquery"],
-
-      // Jasmine-jQuery plugin
-      "jasminejquery": ["jquery"],
-
-      // jQuery Mobile
-      "jquerymobile": ["jquery"],
-
-      // Backbone
-      "backbone": {
-
-          // Lists jQuery and Underscore as dependencies
-          "deps": ["underscore", "jquery"],
-
-          // Exports the global 'window.Backbone' object
-          "exports": "Backbone"
-
-      },
-
-      // Backbone.validateAll depends on Backbone
-      "backbone.validateAll": ["backbone"],
-
-      // Jasmine Unit Testing
-      "jasmine": {
-
-        // Exports the global 'window.jasmine' object
-        "exports": "jasmine"
-
-      },
-
-      // Jasmine Unit Testing helper
-      "jasmine-html": {
-
-        "deps": ["jasmine"],
-
-        "exports": "jasmine"
-
-      }
-
-  }
-
+        // Jasmine Unit Testing helper
+        "jasmine-html": {
+            "deps": ["jasmine"],
+            "exports": "jasmine"
+        }
+    }
 });
 
 // Include Desktop Specific JavaScript files here (or inside of your Desktop router)
-require(["jquery", "backbone", "jasmine-html", "jquerymobile", "bootstrap", "backbone.validateAll"],
+require(["jquery", "backbone", "marionette", "jasmine-html", "jquerymobile", "bootstrap", "backbone.validateAll"],
 
-  function($, Backbone, jasmine) {
+  function($, Backbone, Marionette, jasmine) {
 
     specs = [];
  
--- public/js/test/specs/spec.js
+++ public/js/test/specs/spec.js
@@ -1,35 +1,25 @@
 // Jasmine Unit Testing Suite
 // --------------------------
-define(["jquery", "backbone", "text!templates/heading.html", "views/View","models/Model", "collections/Collection", "routers/DesktopRouter", "routers/MobileRouter", "jasminejquery"],
+define(["jquery", "backbone", "marionette", "models/Model", "collections/Collection", "app/App", "jqueryui", "bootstrap", "backbone.validateAll", "jasminejquery"],
 
-    function($, Backbone, headerText, View, Model, Collection, DesktopRouter, MobileRouter) {
+    function($, Backbone, Marionette, Model, Collection ) {
 
         // Test suite that includes all of the Jasmine unit tests   
-        describe("Backbone-Require-Boilerplate (BRB)", function() {
+        describe("Marionette-Require-Boilerplate (MRB)", function() {
 
             // Backbone View Suite: contains all tests related to views
-            describe("Backbone views", function() {
-
-                // Runs before every View spec
-                beforeEach(function() {
-
-                    // Instantiates a new View instance
-                    this.view = new View();
-
+            describe("Desktop Marionette App instantiation", function() {
+                //Initialize App in Desktop Mode (App is global var)
+                App.start();
+                it("App should start and have Regions", function() {
+                    expect(App.mainRegion.el).toEqual("#main");
+                    expect(App.headerRegion.el).toEqual("header");
                 });
 
-                it("should contain the correct view element", function() {
 
-                    this.router = new DesktopRouter();
-
-                    expect(this.view.$el.selector).toEqual(".example");
-
-                });
-
-                it("should contain the appropriate template", function() {
-
-                    expect(this.view.template).toEqual(headerText);
-
+                it("App Regions should contain views", function() {
+                    expect(App.headerRegion.currentView).toBeDefined();
+                    expect(App.mainRegion.currentView).toBeDefined();
                 });
 
             }); // End of the View test suite
@@ -39,51 +29,39 @@ define(["jquery", "backbone", "text!templates/heading.html", "views/View","model
 
                 // Runs before every Model spec
                 beforeEach(function() {
-
                     // Instantiates a new Model instance
                     this.model = new Model();
 
                     // We are spying on the _validate method to see if it gets called
                     spyOn(Model.prototype, "validate").andCallThrough();
-
                 });
 
                 it("should be in a valid state", function() {
-
                     expect(this.model.isValid()).toBe(true);
-
                 });
 
                 it("should call the validate method when setting a property", function() {
-
                     this.model.set({ example: "test" });
-
                     expect(Model.prototype.validate).toHaveBeenCalled();
-
                 });
 
             }); // End of the Model test suite
 
         // Backbone Collection Suite: contains all tests related to collections
         describe("Backbone collections", function() {
-
             // Runs before every Collection spec
             beforeEach(function() {
-
                 // Instantiates a new Collection instance
                 this.collection = new Collection();
-
             });
 
             it("should contain the correct number of models", function() {
-
                 expect(this.collection.length).toEqual(0);
-
             });
 
         }); // End of the Collection test suite
 
-        // Backbone Desktop Router Suite: contains all tests related to Desktop routers
+        /*// Backbone Desktop Router Suite: contains all tests related to Desktop routers
         describe("Backbone desktop routers", function () {
 
             // Runs before every Desktop Router spec
@@ -152,7 +130,7 @@ define(["jquery", "backbone", "text!templates/heading.html", "views/View","model
             });
 
         }); // End of the Mobile Router test suite
-
+*/
     }); // End of the BRB test suite
 
 });
